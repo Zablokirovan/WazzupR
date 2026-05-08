f98fd7a3ac682bc6c427a0de6cce602299209a66
Zablokirovan <artemiszbobylev05@gmail.com>
2026-05-08 18:08:09 +0500

    Интрукция для работы с проектом

--- db_data.py
+++ db_data.py
@@ -19,11 +19,19 @@ shema = 'MessagingCore'
 
 def get_number_info():
     query = f"""
-  select civ.phone_num
-    from "MessagingCore".v_all_phone_numbers civ
-    where civ.phone_num LIKE '77%'
-    GROUP BY civ.phone_num
-        HAVING COUNT(*) = 1;
+  SELECT ci.phone_num_clear
+FROM "MessagingCore".contact_info ci
+INNER JOIN "MessagingCore".contact_in_deal cid 
+    ON cid.contact_id = ci.contact_id 
+LEFT JOIN "MessagingCore".result_response_messagess rrm 
+    ON rrm.phone_num = ci.phone_num_clear 
+WHERE cid.date_create >= '2025-11-01'
+  AND cid.date_create < '2025-12-31'
+  AND ci.phone_num_clear IS NOT NULL
+  AND rrm.messages_id IS NULL
+GROUP BY ci.phone_num_clear
+HAVING COUNT(DISTINCT ci.contact_id) = 1
+LIMIT 4500;
     """
     with db_client.cursor() as cur:
         cur.execute(query)
@@ -58,5 +66,20 @@ def insert_data_for_messages(info_mess, resp_code, number):
                     status = EXCLUDED.status,
                     error = EXCLUDED.error
             """, info)
+
+        if resp_code == 201:
+            sender_info = [(info_mess["chatId"], 'kaspi_WAZZUP', 'whatsapp')]
+            cur.executemany("""
+                INSERT INTO n8n.sender_map (
+                    sender_id,
+                    chanel_id,
+                    chanel_type
+                )
+                VALUES (%s, %s, %s)
+                ON CONFLICT (sender_id)
+                DO UPDATE SET
+                    chanel_id = EXCLUDED.chanel_id,
+                    chanel_type = EXCLUDED.chanel_type
+            """, sender_info)
 
     db_client.commit()
